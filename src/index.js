9069127f16740c561dd6f96748aa83d5edf3dff6
acheronfail <acheronfail@gmail.com>
2017-12-29 12:38:23 +1100

    visual fixes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './app/app';
 import registerServiceWorker from './registerServiceWorker';
 
-window.app = ReactDOM.render(<App />, document.getElementById('root'));
+window.app = ReactDOM.render(
+    <div>
+        <App />
+        <div className="site-footer">
+            Made with ❤️ by <a href="https://github.com/acheronfail">acheronfail</a>, because <a href="https://alf.nu/ReturnTrue">alf.nu/ReturnTrue</a> is down 😞
+        </div>
+    </div>,
+    document.getElementById('root')
+);
 registerServiceWorker();
