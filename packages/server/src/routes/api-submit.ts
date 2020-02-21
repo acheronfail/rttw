bc7b5c5db19c97606978dd5ae7d6547bcdc9e975
acheronfail <acheronfail@gmail.com>
2020-02-21 22:15:18 +1100

    chore(server): add logging when solutions fail

--- packages/server/src/routes/api-submit.ts
+++ packages/server/src/routes/api-submit.ts
@@ -12,12 +12,15 @@ import { firefox } from 'playwright';
 // TODO: create concept of "services" to hold business logic (and instantiate dependencies).
 const browser = firefox.launch({ headless: true });
 
-// TODO: handle errors (use TestResult here too)
 async function validateSolution(puzzle: Puzzle, userInput: string): Promise<boolean> {
   const context = await (await browser).newContext();
   const webpage = await context.newPage();
   const result = await webpage.evaluate(evalTemplate(puzzle, userInput));
-  return result === true;
+  if (result !== true) {
+    throw new Error(`Returned value was: ${result}`);
+  }
+
+  return true;
 }
 
 export const apiSubmitRoute: ApiSubmitRouteOptions = {
@@ -26,7 +29,6 @@ export const apiSubmitRoute: ApiSubmitRouteOptions = {
   schema: schemas.routes.apiSubmit,
   handler: async function(request, reply): Promise<ApiSubmitResponse> {
     const { id, name, solution } = request.body;
-    console.log(request.body);
 
     const user = await this.store.getOrAddUser(id);
     const limit = constants.VIEWABLE_PUZZLE_COUNT + getSolvedPuzzleCount(user);
@@ -35,13 +37,17 @@ export const apiSubmitRoute: ApiSubmitRouteOptions = {
     const puzzle = await this.store.getPuzzle(name);
     if (puzzle.index >= limit) {
       reply.code(403);
-      throw new Error('User attempted to solve a puzzle without access');
+      throw new Error('Attempted to solve a puzzle without access');
     }
 
-    const isSolutionValid = await validateSolution(puzzle, solution);
+    const isSolutionValid = await validateSolution(puzzle, solution).catch(err => {
+      this.log.info({ err });
+      return false;
+    });
+
     if (!isSolutionValid) {
       reply.code(403);
-      throw new Error(`User solution failed: ${name} ${solution}`);
+      throw new Error(`Solution failed for puzzle: ${name}`);
     }
 
     // Update solution in db
